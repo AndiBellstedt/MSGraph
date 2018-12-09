4a3eadfd4549aed2edacc78a7e232a79ebf55bd2
Andi Bellstedt <bellstedt@gmx.de>
2018-12-09 15:10:14 +0100

    change file encoding

--- MSGraph/internal/configurations/variable.ps1
+++ MSGraph/internal/configurations/variable.ps1
@@ -1,3 +1,3 @@
-# defining module wide variables
+﻿# defining module wide variables
 
 New-Variable -Name msgraph_Token -Scope Script -Visibility Public -Description "Variable for registered token. This is for convinience use with the commands in MSGraph module" -Force
