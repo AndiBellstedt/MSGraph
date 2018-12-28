5669f6bdaeed5b6a005a4aea98a4fc9747e339c4
Andi Bellstedt <bellstedt@gmx.de>
2018-12-28 23:46:44 +0100

    add parameter and fix documentation

--- MSGraph/functions/exchange/category/Get-MgaExchCategory.ps1
+++ MSGraph/functions/exchange/category/Get-MgaExchCategory.ps1
@@ -18,6 +18,8 @@
         The color to filter by.
         (Client Side filtering)
 
+        Tab completion is available on this parameter for the list of the 25 predefined colors.
+
     .PARAMETER User
         The user-account to access. Defaults to the main user connected as.
         Can be any primary email name of any user the connected token has access to.
@@ -31,29 +33,34 @@
         Can be omitted if a connection has been registered using the -Register parameter on New-MgaAccessToken.
 
     .EXAMPLE
-        PS C:\> Get-MgaMailCategories
+        PS C:\> Get-MgaExchCategory
 
         Return all categories of the user connected to through a token.
 
     .EXAMPLE
-        PS C:\> Get-MgaMailCategories -Id "89101089-690d-4263-9470-b674e709a996"
+        PS C:\> Get-MgaExchCategory -Id "89101089-690d-4263-9470-b674e709a996"
 
         Return the category with the specified Id of the user connected to through a token.
 
     .EXAMPLE
-        PS C:\> Get-MgaMailCategories -Name "*category"
+        PS C:\> Get-MgaExchCategory -Name "*category"
 
         Return all categories with names like "*category" of the user connected to through a token.
 
     .EXAMPLE
-        PS C:\> Get-MgaMailCategories -Color "Blue"
+        PS C:\> Get-MgaExchCategory -Color "Blue"
 
         Return all categories with names like "*category" of the user connected to through a token.
 
     #>
     [CmdletBinding(ConfirmImpact = 'Low', DefaultParameterSetName = 'Default')]
-    #[OutputType([MSGraph.Exchange.Category])]
+    [OutputType([MSGraph.Exchange.Category.OutlookCategory])]
     param (
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Position = 0, ParameterSetName = 'ByInputOBject')]
+        [Alias('Category')]
+        [MSGraph.Exchange.Category.CategoryParameter[]]
+        $InputObject,
+
         [Parameter(Mandatory = $true, Position = 0, ParameterSetName = 'ById')]
         [Alias('IdFilter', 'FilterId')]
         [guid[]]
@@ -86,31 +93,54 @@
     process {
         #region query data
         $invokeParam = @{
-            "Field"        = "outlook/masterCategories"
             "Token"        = $Token
             "User"         = $User
             "ResultSize"   = $ResultSize
             "FunctionName" = $MyInvocation.MyCommand
         }
 
-        Write-PSFMessage -Level Verbose -Message "Getting available categories" -Tag "QueryData"
-        $data = Invoke-MgaGetMethod @invokeParam 
+        $data = @()
+        if ($PSCmdlet.ParameterSetName -like 'ByInputOBject') {
+            foreach ($categoryItem in $InputObject) {
+                #region checking input object type and query message if required
+                if ($categoryItem.TypeName -like "System.String") {
+                    $categoryItem = Resolve-MailObjectFromString -Object $categoryItem -User $User -Token $Token -FunctionName $MyInvocation.MyCommand
+                    if (-not $categoryItem) { continue }
+                }
+
+                $User = Resolve-UserInMailObject -Object $categoryItem -User $User -ShowWarning -FunctionName $MyInvocation.MyCommand
+                #endregion checking input object type and query message if required
+
+                $invokeParam.Add("Field","outlook/masterCategories/$($categoryItem.Id)")
+                Write-PSFMessage -Level Verbose -Message "Get refresh on category '$($categoryItem)'" -Tag "QueryData"
+                $data = $data + (Invoke-MgaGetMethod @invokeParam)
+                $invokeParam.Remove("Field")
+            }
+        }
+        else {
+            $invokeParam.Add("Field","outlook/masterCategories")
+            Write-PSFMessage -Level Verbose -Message "Getting available categories" -Tag "QueryData"
+            $data = $data + (Invoke-MgaGetMethod @invokeParam)
+        }
         #endregion query data
 
         #region filter data
         switch ($PSCmdlet.ParameterSetName) {
             'ById' { 
                 $data = foreach ($filter in $Id) {
+                    Write-PSFMessage -Level VeryVerbose -Message "Filtering on id '$($filter)'." -Tag "FilterData"
                     $data | Where-Object Id -like $filter.Guid
                 }
             }
             'ByName' {
                 $data = foreach ($filter in $Name) {
+                    Write-PSFMessage -Level VeryVerbose -Message "Filtering on name '$($filter)'." -Tag "FilterData"
                     $data | Where-Object displayname -like $filter
                 }
             }
             'ByColor' {
                 $data = foreach ($filter in $Color) {
+                    Write-PSFMessage -Level VeryVerbose -Message "Filtering on color '$($filter)'." -Tag "FilterData"
                     $data | Where-Object Color -like ([MSGraph.Exchange.Category.OutlookCategory]::Parse($filter))
                 }
             }
@@ -119,9 +149,11 @@
         #endregion filter data
 
         #region output data
+        Write-PSFMessage -Level VeryVerbose -Message "Output $($data.Count) objects." -Tag "OutputData"
         foreach ($output in $data) {
             if($output.User) { $User = $output.User }
             $categoryObject = [MSGraph.Exchange.Category.OutlookCategory]::new( $output.id, $output.displayName, $output.color, $User, $output)
+            Write-PSFMessage -Level Debug -Message "Output new object '$($categoryObject)'." -Tag "OutputData"
             $categoryObject
         }
         #endregion output data
