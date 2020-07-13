cdf36bec0b3b5260e51f0f614fd5c2d2c5d69e1d
Andi Bellstedt <bellstedt@gmx.de>
2020-07-13 21:31:13 +0200

    generalizing CI/CD scripts for ease of configuration

--- build/vsts-prerequisites.ps1
+++ build/vsts-prerequisites.ps1
@@ -1,6 +1,44 @@
-﻿Write-Host "Installing Pester" -ForegroundColor Cyan
-Install-Module Pester -Force -SkipPublisherCheck
-Write-Host "Installing PSFramework" -ForegroundColor Cyan
-Install-Module PSFramework -Force -SkipPublisherCheck
-Write-Host "Installing PSScriptAnalyzer" -ForegroundColor Cyan
-Install-Module PSScriptAnalyzer -Force -SkipPublisherCheck
+﻿<#
+    .SYNOPSIS
+        This script ensures prerequisites to run tests
+
+    .DESCRIPTION
+        This script ensures prerequisites to run tests
+
+    .PARAMETER ModuleName
+        The name to give to the module.
+
+    .PARAMETER Repository
+        The name of the repository to publish to.
+        Defaults to PSGallery.
+
+#>
+param (
+    [string]
+    $ModuleName,
+
+    [string]
+    $Repository = 'PSGallery',
+
+    $Modules = @("Pester", "PSModuleDevelopment", "PSScriptAnalyzer")
+)
+
+
+# Automatically add missing dependencies
+$data = Import-PowerShellDataFile -Path "$PSScriptRoot\..\$($ModuleName)\$($ModuleName).psd1"
+foreach ($dependency in $data.RequiredModules) {
+    if ($dependency -is [string]) {
+        if ($Modules -contains $dependency) { continue }
+        $Modules += $dependency
+    }
+    else {
+        if ($Modules -contains $dependency.ModuleName) { continue }
+        $Modules += $dependency.ModuleName
+    }
+}
+
+foreach ($module in $Modules) {
+    Write-Host "Installing $module" -ForegroundColor Cyan
+    Install-Module $module -Force -SkipPublisherCheck -Repository $Repository
+    Import-Module $module -Force -PassThru
+}
